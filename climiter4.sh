8e850b433a97ac1977960eeffeaa2f438d7a0756
IGNAS8 <34920434+IGNAS8@users.noreply.github.com>
2017-12-28 19:52:11 +0200

    upd

---
+++ climiter4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cpu=`cat cpuname.txt`
+while [ 1=1 ]
+do
+	wait=$(shuf -i2-6 -n1)
+	load=$(shuf -i236-292 -n1)
+	cpulimit -e ${cpu} -l ${load} -b -q
+	sleep ${wait}
+	pkill cpulimit
+done
